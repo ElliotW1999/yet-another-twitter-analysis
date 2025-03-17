998b0a2d98d893613992850af210e57400b764d8
Elliot Warner <48418365+ElliotW1999@users.noreply.github.com>
2025-03-17 22:42:16 +1300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,9 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region     = "us-west-2"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 resource "aws_instance" "twitter_data" {
