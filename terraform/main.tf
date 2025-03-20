e279ecca9e10807cb9b1b4f8f0e954a197a13170
Elliot Warner <48418365+ElliotW1999@users.noreply.github.com>
2025-03-20 23:01:48 +1300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,16 @@ resource "aws_sqs_queue" "twitter-analysis-queue" {
   })
 }
 
+resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.terraform_queue_deadletter.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.terraform_queue.arn]
+  })
+}
+
+
 variable "subnet" {
   description = "The subnet ID"
   type        = string
