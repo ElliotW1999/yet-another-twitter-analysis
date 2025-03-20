db02b2becd90ece7a0a04d5105f7050555b89a17
Elliot Warner <48418365+ElliotW1999@users.noreply.github.com>
2025-03-20 23:15:52 +1300

    sqs hotfix

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,21 +43,21 @@ resource "aws_sqs_queue" "twitter-analysis-queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    deadLetterTargetArn = aws_sqs_queue.twitter_data_queue_deadletter.arn
     maxReceiveCount     = 4
   })
 }
 
-resource "aws_sqs_queue" "terraform_queue_deadletter" {
-  name = "terraform -example-deadletter-queue"
+resource "aws_sqs_queue" "twitter_data_queue_deadletter" {
+  name = "twitter-data-deadletter-queue"
 }
 
-resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
-  queue_url = aws_sqs_queue.terraform_queue_deadletter.id
+resource "aws_sqs_queue_redrive_allow_policy" "twitter_data_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.twitter_data_queue_deadletter.id
 
   redrive_allow_policy = jsonencode({
     redrivePermission = "byQueue",
-    sourceQueueArns   = [aws_sqs_queue.terraform_queue.arn]
+    sourceQueueArns   = [aws_sqs_queue.twitter_data_queue.arn]
   })
 }
 
